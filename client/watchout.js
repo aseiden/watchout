82d050fd878989085c8c18eda3afad9b4df9a63f
MKS-Student <mks.student@hackreactor.com>
2017-03-31 16:06:27 -0700

    move the ship when dragged

--- client/watchout.js
+++ client/watchout.js
@@ -8,6 +8,8 @@ var board = d3.select('.board')
               .style('background-color', 'black');
 
 var asteroids = 'abcdefg'.split('');
+var ship = [{key: 'theLoneWanderer', x: 0, y: 0}];
+var drag = d3.behavior.drag();
 
 function update(data) {
   board.selectAll('.asteroid')
@@ -16,8 +18,20 @@ function update(data) {
     .duration(1000)
     .attr('x', function() { return Math.random() * width; })
     .attr('y', function() { return Math.random() * height; });
-
 }
+
+
+var drag = d3.behavior.drag()
+        .on("drag", function(d,i) {
+            debugger;
+            d.x += d3.event.dx
+            d.y += d3.event.dy
+            d3.select(this).attr("transform", function(d,i){
+                return "translate(" + [ d.x,d.y ] + ")"
+            })
+        });
+
+        // return "translate(" + [ d.x,d.y ] + ")"
 
 board.selectAll('.asteroid')
      .data(asteroids)
@@ -32,3 +46,21 @@ board.selectAll('.asteroid')
 
 update(asteroids);
 setInterval(function(){ update(asteroids); }, 1000);
+
+board.selectAll('.ship')
+     .data(ship)
+     .enter()
+     .append('svg:image')
+     .attr('class', 'ship')
+     .attr('xlink:href', 'spacestation.png')
+     .attr('width', 40)
+     .attr('height', 40)
+     .attr('x', function() { return width / 2; })
+     .attr('y', function() { return height / 2; })
+     .call(drag);
+
+// d3.select('.ship').on("click", function(d) { // ignore drag
+//   // d3.select('.ship').call(force.drag);
+//   alert('AHHHHH');
+// });
+
